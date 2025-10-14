2c75b24077a6ac9b209f31e53952b1666a91b108
JackBinary <JMG.EXE@gmail.com>
2025-10-14 14:25:55 -0500

    Update actors.js

--- module/actors.js
+++ module/actors.js
@@ -1,58 +1,61 @@
+// module/actors.js
+console.log("GFL5R | actors.js loaded");
+
 export class GFL5RActorSheet extends ActorSheet {
   static get defaultOptions() {
     const opts = super.defaultOptions;
     return foundry.utils.mergeObject(opts, {
       classes: ["gfl5r", "sheet", "actor"],
-      width: 700,
-      height: 650,
-      // You kept a single "Skills" tab; header is above .sheet-body, which is fine.
+      width: 860,
+      height: 700,
       tabs: [{ navSelector: ".tabs", contentSelector: ".sheet-body", initial: "skills" }]
     });
   }
 
   get template() {
-    return "systems/gfl5r/templates/actor-sheet.html";
+    return `systems/${game.system.id}/templates/actor-sheet.html`;
   }
 
-  /** v12+: async getData */
   async getData(options) {
+    console.log("GFL5R | getData()");
     const context = await super.getData(options);
     const data = context.actor.system ?? {};
 
     // Approaches
-    const power = data.approaches?.power ?? 0;
-    const swiftness = data.approaches?.swiftness ?? 0;
+    const power      = data.approaches?.power ?? 0;
+    const swiftness  = data.approaches?.swiftness ?? 0;
     const resilience = data.approaches?.resilience ?? 0;
-    const precision = data.approaches?.precision ?? 0;
-    const fortune = data.approaches?.fortune ?? 0;
+    const precision  = data.approaches?.precision ?? 0;
+    const fortune    = data.approaches?.fortune ?? 0;
 
-    // Derived attributes
-    const endurance  = (power + resilience) * 2;
-    const composure  = (resilience + swiftness) * 2;
-    const vigilance  = Math.ceil((precision + swiftness) / 2);
-    const focus      = power + precision;
-    const fpMax      = fortune;
+    // Derived
+    const endurance = (power + resilience) * 2;
+    const composure = (resilience + swiftness) * 2;
+    const vigilance = Math.ceil((precision + swiftness) / 2);
+    const focus     = power + precision;
 
-    // Current FP is now persistent at system.resources.fortunePoints.
-    // If you want a lazy default when the saved value is undefined or null, fall back to half Fortune.
-    const fpCurrent  = (data.resources?.fortunePoints ?? Math.floor(fpMax / 2));
+    const fpMax     = fortune;
+    const fpCurrent = (data.resources?.fortunePoints ?? Math.floor(fpMax / 2));
 
     context.derived = {
-      endurance,
-      composure,
-      vigilance,
-      focus,
+      endurance, composure, vigilance, focus,
       fortunePointsMax: fpMax,
       fortunePointsCurrent: fpCurrent
     };
 
-    // expose skills for convenience if you loop later
+    // Expose skills & abilities
     context.skills = data.skills ?? {};
+    context.abilities = this.actor.items.filter(i => i.type === "ability").map(i => ({
+      id: i.id,
+      name: i.name,
+      img: i.img,
+      system: i.system ?? {}
+    }));
+
     return context;
   }
-}
 
-activateListeners(html) {
+  activateListeners(html) {
     super.activateListeners(html);
     console.log("GFL5R | activateListeners()");
 
