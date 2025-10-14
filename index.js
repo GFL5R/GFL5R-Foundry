4fadb7856e19372a9ace8b399745768ef498b454
JackBinary <JMG.EXE@gmail.com>
2025-10-14 12:40:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+class BarebonesActorSheet extends ActorSheet {
+  get template() {
+    return "systems/gfl5r/templates/actor-sheet.html";
+  }
+
+  getData() {
+    const context = super.getData();
+    const data = context.actor.system;
+
+    // Derived attributes
+    const power = data.approaches.power || 0;
+    const swiftness = data.approaches.swiftness || 0;
+    const resilience = data.approaches.resilience || 0;
+    const precision = data.approaches.precision || 0;
+    const fortune = data.approaches.fortune || 0;
+
+    context.derived = {
+      endurance: (power + resilience) * 2,
+      composure: (resilience + swiftness) * 2,
+      vigilance: Math.ceil((precision + swiftness) / 2),
+      focus: power + precision,
+      fortunePointsMax: fortune,
+      fortunePointsCurrent: Math.floor(fortune / 2)
+    };
+
+    return context;
+  }
+}
+
 Hooks.once("init", () => {
-  console.log("Barebones init");
+  console.log("GFL5R | Initializing");
+  Actors.unregisterSheet("core", ActorSheet);
+  Actors.registerSheet("gfl5r", BarebonesActorSheet, { makeDefault: true });
 });
