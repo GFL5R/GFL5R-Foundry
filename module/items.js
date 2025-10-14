f01d69d11973378967d544c80fbd8ff938501263
JackBinary <JMG.EXE@gmail.com>
2025-10-14 15:34:20 -0500

    Update items.js

--- module/items.js
+++ module/items.js
@@ -8,18 +8,38 @@ export class GFL5RAbilitySheet extends ItemSheet {
     });
   }
 
-  get template() {
-    return "systems/gfl5r/templates/item-ability.html";
+  get template() { return "systems/gfl5r/templates/item-ability.html"; }
+}
+
+export class GFL5RWeaponSheet extends ItemSheet {
+  static get defaultOptions() {
+    const opts = super.defaultOptions;
+    return foundry.utils.mergeObject(opts, {
+      classes: ["gfl5r", "sheet", "item"],
+      width: 600,
+      height: 550
+    });
   }
 
-  async getData(options) {
-    const context = await super.getData(options);
-    context.system = context.item.system ?? {};
-    return context;
+  get template() { return "systems/gfl5r/templates/item-weapon.html"; }
+}
+
+export class GFL5RArmorSheet extends ItemSheet {
+  static get defaultOptions() {
+    const opts = super.defaultOptions;
+    return foundry.utils.mergeObject(opts, {
+      classes: ["gfl5r", "sheet", "item"],
+      width: 600,
+      height: 500
+    });
   }
+
+  get template() { return "systems/gfl5r/templates/item-armor.html"; }
 }
 
 export function registerItemSheets() {
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("gfl5r", GFL5RAbilitySheet, { types: ["ability"], makeDefault: true });
+  Items.registerSheet("gfl5r", GFL5RWeaponSheet, { types: ["weaponry"], makeDefault: true });
+  Items.registerSheet("gfl5r", GFL5RArmorSheet, { types: ["armor"], makeDefault: true });
 }
