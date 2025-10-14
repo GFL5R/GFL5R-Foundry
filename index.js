99a6f09ef6b24519b7fad4e021dd2292de05656d
JackBinary <JMG.EXE@gmail.com>
2025-10-14 12:41:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-class BarebonesActorSheet extends ActorSheet {
+class GFL5RActorSheet extends ActorSheet {
   get template() {
     return "systems/gfl5r/templates/actor-sheet.html";
   }
 
-  getData() {
-    const context = super.getData();
-    const data = context.actor.system;
+  /** v12-friendly: super.getData() is async */
+  async getData(options) {
+    const context = await super.getData(options);
+    const data = context.actor.system ?? {};
+
+    // Approaches (default to 0 if unset)
+    const power = data.approaches?.power ?? 0;
+    const swiftness = data.approaches?.swiftness ?? 0;
+    const resilience = data.approaches?.resilience ?? 0;
+    const precision = data.approaches?.precision ?? 0;
+    const fortune = data.approaches?.fortune ?? 0;
 
     // Derived attributes
-    const power = data.approaches.power || 0;
-    const swiftness = data.approaches.swiftness || 0;
-    const resilience = data.approaches.resilience || 0;
-    const precision = data.approaches.precision || 0;
-    const fortune = data.approaches.fortune || 0;
-
     context.derived = {
       endurance: (power + resilience) * 2,
       composure: (resilience + swiftness) * 2,
       vigilance: Math.ceil((precision + swiftness) / 2),
       focus: power + precision,
       fortunePointsMax: fortune,
-      fortunePointsCurrent: Math.floor(fortune / 2)
+      fortunePointsCurrent: Math.floor(fortune / 2) // display-only default
     };
 
     return context;
@@ -30,5 +32,5 @@ class BarebonesActorSheet extends ActorSheet {
 Hooks.once("init", () => {
   console.log("GFL5R | Initializing");
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("gfl5r", BarebonesActorSheet, { makeDefault: true });
+  Actors.registerSheet("gfl5r", GFL5RActorSheet, { makeDefault: true });
 });
