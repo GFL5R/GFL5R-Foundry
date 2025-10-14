873e30ea67eb209d03d45c6465031e7eae3efe5e
JackBinary <JMG.EXE@gmail.com>
2025-10-14 18:39:23 -0500

    Update main.js

--- module/main.js
+++ module/main.js
@@ -4,6 +4,12 @@ import { registerItemSheets } from "./items.js";
 Hooks.once("init", () => {
   console.log("GFL5R | Initializing");
 
+  Handlebars.registerHelper("object", function () {
+    const out = {};
+    for (let i = 0; i < arguments.length - 1; i += 2) out[arguments[i]] = arguments[i + 1];
+    return out;
+  });
+
   registerActorSheets();
   registerItemSheets();
 });
