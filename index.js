0f5fb41e3ce0f65d2a14b0f81cc5fbec688152df
JackBinary <JMG.EXE@gmail.com>
2025-10-14 12:53:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 class GFL5RActorSheet extends ActorSheet {
+  static get defaultOptions() {
+    const opts = super.defaultOptions;
+    // Classic Foundry tabs config (works in v12)
+    return foundry.utils.mergeObject(opts, {
+      classes: ["gfl5r", "sheet", "actor"],
+      width: 640,
+      height: 600,
+      tabs: [{ navSelector: ".tabs", contentSelector: ".sheet-body", initial: "skills" }]
+    });
+  }
+
   get template() {
     return "systems/gfl5r/templates/actor-sheet.html";
   }
 
-  /** v12-friendly: super.getData() is async */
   async getData(options) {
     const context = await super.getData(options);
     const data = context.actor.system ?? {};
 
-    // Approaches (default to 0 if unset)
     const power = data.approaches?.power ?? 0;
     const swiftness = data.approaches?.swiftness ?? 0;
     const resilience = data.approaches?.resilience ?? 0;
     const precision = data.approaches?.precision ?? 0;
     const fortune = data.approaches?.fortune ?? 0;
 
-    // Derived attributes
     context.derived = {
       endurance: (power + resilience) * 2,
       composure: (resilience + swiftness) * 2,
       vigilance: Math.ceil((precision + swiftness) / 2),
       focus: power + precision,
       fortunePointsMax: fortune,
-      fortunePointsCurrent: Math.floor(fortune / 2) // display-only default
+      fortunePointsCurrent: Math.floor(fortune / 2)
     };
 
+    // For convenience in template loops
+    context.skills = data.skills ?? {};
     return context;
   }
 }
