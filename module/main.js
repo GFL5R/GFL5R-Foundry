8b9e59e3a149bd967e3cbdc2f5197170b2f8f514
JackBinary <jmg.exe@gmail.com>
2025-11-19 22:12:19 -0600

    Enhance Handlebars conditional helper and bump version to 0.1.13

--- module/main.js
+++ module/main.js
@@ -20,6 +20,33 @@ Hooks.once("init", () => {
 
   Handlebars.registerHelper("eq", (a, b) => a === b);
 
+  Handlebars.registerHelper("ifCond", function(v1, operator, v2, options) {
+    switch (operator) {
+      case '==':
+        return (v1 == v2) ? options.fn(this) : options.inverse(this);
+      case '===':
+        return (v1 === v2) ? options.fn(this) : options.inverse(this);
+      case '!=':
+        return (v1 != v2) ? options.fn(this) : options.inverse(this);
+      case '!==':
+        return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+      case '<':
+        return (v1 < v2) ? options.fn(this) : options.inverse(this);
+      case '<=':
+        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+      case '>':
+        return (v1 > v2) ? options.fn(this) : options.inverse(this);
+      case '>=':
+        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+      case '&&':
+        return (v1 && v2) ? options.fn(this) : options.inverse(this);
+      case '||':
+        return (v1 || v2) ? options.fn(this) : options.inverse(this);
+      default:
+        return options.inverse(this);
+    }
+  });
+
   // Register settings
   registerSettings();
 
